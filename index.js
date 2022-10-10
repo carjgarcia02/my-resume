149ffc7b8ff44f960af17d078aa219c99667c65a
carjgarcia02 <carjgarcia02@gmail.com>
2022-10-10 11:00:03 -0500

    Added responsive design

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const setDarkTheme = () => {
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")){
         icon.src = "./img/sun.png";
+        icon.style.border = "2px solid #f8a805";
+        icon.style.title = "Switch to normal mode"
     } else {
         icon.src = "./img/moon.jpg";
+        icon.style.border = "2px solid black";
+        icon.style.title = "Switch to dark mode"
     }
 }
